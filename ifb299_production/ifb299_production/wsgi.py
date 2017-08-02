610bec593e5a15e714e4540db592aa255ded6d16
root <root@ip-172-31-3-175.ap-southeast-2.compute.internal>
2017-08-02 08:53:28 +0000

    django project

---
+++ ifb299_production/ifb299_production/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ifb299_production project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ifb299_production.settings")
+
+application = get_wsgi_application()
