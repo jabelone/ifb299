81077e864e3871864d74920b30e2e943d8072cef
DESKTOP-FS3M7K4\JHY <joongheumyoon@gmail.com>
2017-09-16 02:57:33 +1000

    designed html and css files and enabled to display it on the web application

--- ifb299_production/ifb299_production/settings.py
+++ ifb299_production/ifb299_production/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'production',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'ifb299_production.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #dir written by jy
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#written by jy
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- ifb299_production/production/views.py
+++ ifb299_production/production/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+#def index(request):
+#    return HttpResponse("Hello, world. You've hit the 'production' 'app' in django.")
 
 
 def index(request):
-    return HttpResponse("Hello, world. You've hit the 'production' 'app' in django.")
-
-
+    return render(request, 'production/home.html')
 
 
 
