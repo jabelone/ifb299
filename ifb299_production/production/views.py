b9711bfe8fc077542d44373a4688afe89ed0bae6
darthjeditt <trai4.t@gmail.com>
2017-09-20 20:19:47 +1000

    Created the registration page
    Made a draft version of the registration page, will make it better later but for now its simply and easy enough to work with

--- ifb299_production/production/views.py
+++ ifb299_production/production/views.py
@@ -1,27 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 #def index(request):
 #    return HttpResponse("Hello, world. You've hit the 'production' 'app' in django.")
 
 
 def index(request):
     return render(request, 'production/SignIn.html')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
