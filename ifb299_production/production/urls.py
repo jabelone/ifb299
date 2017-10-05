75609216895745ee1604360ca63f284e7542f723
jabelone <hello@jaimyn.com.au>
2017-10-05 19:40:05 +1000

    updated URLS

--- ifb299_production/production/urls.py
+++ ifb299_production/production/urls.py
@@ -3,8 +3,10 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
-    url(r'^registration/$', views.registration, name='registration'),
+    url(r'^signin/$', auth_views.login, name='signin'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/loggedout'}, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^loggedin/$', views.loggedin, name='loggedin'),
+    url(r'^loggedout/$', views.loggedout, name='loggedout'),
     url(r'^$', views.index, name='index'),
 ]
