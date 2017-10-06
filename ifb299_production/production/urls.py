d3f7065371904ed9f2345e76a7b846e23391d998
jabelone <hello@jaimyn.com.au>
2017-10-06 13:52:17 +1000

    Changed default page and renamed signout

--- ifb299_production/production/urls.py
+++ ifb299_production/production/urls.py
@@ -4,9 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^signin/$', auth_views.login, name='signin'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/loggedout'}, name='logout'),
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^loggedin/$', views.loggedin, name='loggedin'),
+    url(r'^signout/$', auth_views.logout, {'next_page': '/loggedout'}, name='logout'),
     url(r'^loggedout/$', views.loggedout, name='loggedout'),
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
 ]
