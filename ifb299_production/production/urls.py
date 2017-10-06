4f5ec280a4151ed698b7365a1c513c6793f99d49
darthjeditt <trai4.t@gmail.com>
2017-10-06 14:01:47 +1000

    test
    test

--- ifb299_production/production/urls.py
+++ ifb299_production/production/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^signin/$', auth_views.login, name='signinnnnnnnnnn'),
+    url(r'^signin/$', auth_views.login, name='signin'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^signout/$', auth_views.logout, {'next_page': '/loggedout'}, name='logout'),
     url(r'^loggedout/$', views.loggedout, name='loggedout'),
