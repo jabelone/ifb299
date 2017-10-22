4744501dce211a37247a31682f2a258047ee03c1
Unknown <trai4.t@gmail.com>
2017-10-22 16:53:31 +1000

    Revert "fixes"
    This reverts commit 2b038c65766383b4316edf276c04d5f7b89e777e.

--- ifb299_production/production/forms.py
+++ ifb299_production/production/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 from production.models import *
 
+
 class SignUpForm(UserCreationForm):
     user_types = forms.ChoiceField(choices=((None, ''), ('student', 'STUDENT'), ('tourist', 'TOURIST'), ('business', 'BUSINESS')))
     class Meta:
--- ifb299_production/production/views.py
+++ ifb299_production/production/views.py
@@ -31,41 +31,29 @@ def signup(request):
 
     return render(request, 'production/signup.html', {'user_form': user_form, 'profile_form': profile_form})
 
-def search(request):
-    search_form = ProfileForm()
-    return render(request, 'production/search.html', {'search_form': search_form})
-
 def signin(request):
     return render(request, 'registration/login.html')
-
 
 def error(request):
     return HttpResponse("Oops. This shouldn't be possible. You've entered a URL that's not valid.")
 
-
 def home(request):
     return render(request, 'production/home.html')
-
 
 def loggedout(request):
     return render(request, 'production/loggedout.html')
 
-
 def contacts(request):
     return render(request, 'production/contacts.html')
-
 
 def howto(request):
     return render(request, 'production/howto.html')
 
-
 def whatif(request):
     return render(request, 'production/whatif.html')
 
-
 def cani(request):
     return render(request, 'production/cani.html')
-
 
 def admins(request):
     if request.method == 'POST':
